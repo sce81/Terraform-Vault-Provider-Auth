a43c35096eb2f59b19df8f374fa0ed797190934b
Simon Elliott <simon.elliott@hashicorp.com>
2025-10-24 14:58:00 +0100

    develop - added additional variable set env var & readme

--- main.tf
+++ main.tf
@@ -5,34 +5,50 @@ resource "tfe_workspace" "my_workspace" {
   organization = var.tfc_organization_name
   project_id   = data.tfe_project.tfc_project.id
 }
+
+resource "tfe_variable_set" "vault-auth" {
+  name          = var.variable_set_name
+  description   = "Some description"
+  organization  = var.tfc_organization_name
+}
+
 resource "tfe_variable" "enable_vault_provider_auth" {
-  workspace_id = tfe_workspace.my_workspace.id
+  //workspace_id = tfe_workspace.my_workspace.id
 
   key      = "TFC_VAULT_PROVIDER_AUTH"
   value    = "true"
   category = "env"
-
+  variable_set_id = tfe_variable_set.vault-auth.id
   description = "Enable the Workload Identity integration for Vault."
 }
 resource "tfe_variable" "tfc_vault_addr" {
-  workspace_id = tfe_workspace.my_workspace.id
+  //workspace_id = tfe_workspace.my_workspace.id
 
   key       = "TFC_VAULT_ADDR"
   value     = var.vault_url
   category  = "env"
-  sensitive = true
-
+  variable_set_id = tfe_variable_set.vault-auth.id
   description = "The address of the Vault instance runs will access."
 }
 
 resource "tfe_variable" "tfc_vault_role" {
-  workspace_id = tfe_workspace.my_workspace.id
+  //workspace_id = tfe_workspace.my_workspace.id
 
   key      = "TFC_VAULT_RUN_ROLE"
   value    = vault_jwt_auth_backend_role.tfc_role.role_name
   category = "env"
-
+  variable_set_id = tfe_variable_set.vault-auth.id
   description = "The Vault role runs will use to authenticate."
+}
+
+resource "tfe_variable" "tfc_vault_namespace" {
+  //workspace_id = tfe_workspace.my_workspace.id
+
+  key      = "TFC_VAULT_NAMESPACE"
+  value    = var.vault_namespace
+  category = "env"
+  variable_set_id = tfe_variable_set.vault-auth.id
+  description = "Sets the variable set env variable for which namespace to operate at"
 }
 
 ## Vault Configuration
@@ -57,7 +73,7 @@ resource "vault_jwt_auth_backend_role" "tfc_role" {
   bound_audiences   = [var.tfc_vault_audience]
   bound_claims_type = "glob"
   bound_claims = {
-    sub = "organization:${var.tfc_organization_name}:project:${var.tfc_project_name}:workspace:${var.tfc_workspace_name}:run_phase:*"
+    sub = "organization:${var.tfc_organization_name}:project:${var.target_tfc_project}:workspace:*:run_phase:*"
   }
   user_claim = "terraform_full_workspace"
   role_type  = "jwt"
