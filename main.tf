eb916d8eb03732d1aa4b6a064bd7174355ff8af2
Simon Elliott <simon.elliott@hashicorp.com>
2025-10-28 12:45:49 +0000

    initial testing

--- main.tf
+++ main.tf
@@ -1,73 +1,6 @@
-## HCP Terraform Configuration
-
-resource "tfe_workspace" "my_workspace" {
-  name         = var.tfc_workspace_name
-  organization = var.tfc_organization_name
-  project_id   = data.tfe_project.tfc_project.id
-}
-
-resource "tfe_variable_set" "vault-auth" {
-  name          = var.variable_set_name
-  description   = "Some description"
-  organization  = var.tfc_organization_name
-}
-
-resource "tfe_variable" "enable_vault_provider_auth" {
-  //workspace_id = tfe_workspace.my_workspace.id
-
-  key      = "TFC_VAULT_PROVIDER_AUTH"
-  value    = "true"
-  category = "env"
-  variable_set_id = tfe_variable_set.vault-auth.id
-  description = "Enable the Workload Identity integration for Vault."
-}
-resource "tfe_variable" "tfc_vault_addr" {
-  //workspace_id = tfe_workspace.my_workspace.id
-
-  key       = "TFC_VAULT_ADDR"
-  value     = var.vault_url
-  category  = "env"
-  variable_set_id = tfe_variable_set.vault-auth.id
-  description = "The address of the Vault instance runs will access."
-}
-
-resource "tfe_variable" "tfc_vault_role" {
-  //workspace_id = tfe_workspace.my_workspace.id
-
-  key      = "TFC_VAULT_RUN_ROLE"
-  value    = vault_jwt_auth_backend_role.tfc_role.role_name
-  category = "env"
-  variable_set_id = tfe_variable_set.vault-auth.id
-  description = "The Vault role runs will use to authenticate."
-}
-
-resource "tfe_variable" "tfc_vault_namespace" {
-  //workspace_id = tfe_workspace.my_workspace.id
-
-  key      = "TFC_VAULT_NAMESPACE"
-  value    = var.vault_namespace
-  category = "env"
-  variable_set_id = tfe_variable_set.vault-auth.id
-  description = "Sets the variable set env variable for which namespace to operate at"
-}
-
-## Vault Configuration
-
-resource "vault_jwt_auth_backend" "tfc_jwt" {
-  path               = var.jwt_backend_path
-  type               = "jwt"
-  oidc_discovery_url = "https://${var.tfc_hostname}"
-  bound_issuer       = "https://${var.tfc_hostname}"
-
-  # If you are using TFE with custom / self-signed CA certs you may need to provide them via the
-  # below argument as a string in PEM format.
-  #
-  # oidc_discovery_ca_pem = "my CA certs as PEM"
-}
-
 resource "vault_jwt_auth_backend_role" "tfc_role" {
-  backend        = vault_jwt_auth_backend.tfc_jwt.path
-  role_name      = "tfc-role"
+  backend        = var.jwt_backend_path
+  role_name      = var.role_name
   token_policies = [vault_policy.tfc_policy.name]
 
   bound_audiences   = [var.tfc_vault_audience]
@@ -81,7 +14,8 @@ resource "vault_jwt_auth_backend_role" "tfc_role" {
 }
 
 resource "vault_policy" "tfc_policy" {
-  name = "tfc-policy"
+  name = var.role_name
 
   policy = var.vault_policy
 }
+
--- variables.tf
+++ variables.tf
@@ -1,59 +1,35 @@
-variable "vault_url" {
-  type        = string
-  description = "The URL of the Vault instance you'd like to use with Terraform Cloud"
+variable "role_name" {
+  type = string
+  default     = "terraform-role"
+  description = "Name of jwt auth backend role"
 }
-
-variable "jwt_backend_path" {
-  type        = string
-  default     = "jwt"
-  description = "The path at which you'd like to mount the jwt auth backend in Vault"
-}
-
 variable "tfc_vault_audience" {
   type        = string
   default     = "vault.workload.identity"
   description = "The audience value to use in run identity tokens"
 }
-
-variable "tfc_hostname" {
-  type        = string
-  default     = "app.terraform.io"
-  description = "The hostname of the TFC or TFE instance you'd like to use with Vault"
-}
-
 variable "tfc_organization_name" {
   type        = string
   description = "The name of your Terraform Cloud organization"
-}
-
-variable "tfc_project_name" {
-  type        = string
-  default     = "Default Project"
-  description = "The project under which a workspace will be created"
-}
-
-variable "tfc_workspace_name" {
-  type        = string
-  default     = "my-vault-workspace"
-  description = "The name of the workspace that you'd like to create and connect to Vault"
 }
 variable "target_tfc_project" {
   type        = string
   default     = "vault-configuration"
   description = "The project under which the vault authentication will be valid"
 }
+variable "tfc_project_name" {
+  type        = string
+  default     = "Default Project"
+  description = "The project under which a workspace will be created"
+}
 variable "vault_policy" {
   type        = string
   default     = null
   description = "Vault Policy to assiciate with Terraform Platform"
 }
-variable "vault_namespace" {
+
+variable "jwt_backend_path" {
   type        = string
-  default     = "admin"
-  description = "Vault Namespace to assiciate with Terraform Platform"
-}
-variable "variable_set_name" {
-  type        = string
-  default     = "HCP Terraform to Vault Auth"
-  description = "HCP Terraform Variable Set Name"
+  default     = "jwt"
+  description = "Vault auth backend to associate role with"
 }
